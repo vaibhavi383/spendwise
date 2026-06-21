2d5ff03176ded8cf3c16cc22af918048fd840784
Vaibhavi Kshirsagar <vaibhavik383@gmail.com>
2026-06-21 19:01:39 +0530

    Added custom exception handling for user not found

---
+++ src/main/java/com/vaibhavi/spendwise/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.vaibhavi.spendwise.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(
+            UserNotFoundException ex) {
+
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND);
+    }
+}
--- src/main/java/com/vaibhavi/spendwise/service/UserService.java
+++ src/main/java/com/vaibhavi/spendwise/service/UserService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.vaibhavi.spendwise.exception.UserNotFoundException;
 import com.vaibhavi.spendwise.entity.User;
 import com.vaibhavi.spendwise.repository.UserRepository;
 
@@ -23,9 +23,11 @@ public class UserService {
     }
     
     public User getUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
+
+        return userRepository.findById(id)
+                .orElseThrow(() ->
+                        new UserNotFoundException("User not found with id " + id));
     }
-    
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
